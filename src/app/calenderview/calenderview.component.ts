a235eab5d23b10554227eaed13f32ca1e442b2e9
jayantnis <jayantnishad34@gmail.com>
2022-09-09 01:02:17 +0530

    first commit

---
+++ src/app/calenderview/calenderview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calenderview',
+  templateUrl: './calenderview.component.html',
+  styleUrls: ['./calenderview.component.css']
+})
+export class CalenderviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
